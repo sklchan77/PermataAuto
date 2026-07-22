c41cbff1cbe1222091bcf6c2cfa1c0786ae095b0
sklchan77 <163995725+sklchan77@users.noreply.github.com>
2026-07-23 02:33:20 +0800

    Update App.java

--- depends/utils/src/main/java/my/app/utils/app/App.java
+++ depends/utils/src/main/java/my/app/utils/app/App.java
@@ -75,7 +75,7 @@ public class App extends android.app.Application {
 	}
 
 	public int getLogRotateThreshold() {
-		return 1024 * 1024;
+		return 512 * 1024;
 	}
 
 	public HandlerExecutor getHandler() {
