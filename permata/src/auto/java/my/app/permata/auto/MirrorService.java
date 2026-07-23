7ec91b40a7e84daa6859fbb88b499aa0021966ad
sklchan77 <163995725+sklchan77@users.noreply.github.com>
2026-07-23 16:22:32 +0800

    Update MirrorService.java

--- permata/src/auto/java/my/app/permata/auto/MirrorService.java
+++ permata/src/auto/java/my/app/permata/auto/MirrorService.java
@@ -3,6 +3,7 @@ package my.app.permata.auto;
 import android.content.Intent;
 import com.google.android.apps.auto.sdk.CarActivity;
 import com.google.android.apps.auto.sdk.CarActivityService;
+import my.app.permata.PermataApplication;
 import my.app.utils.log.Log;
 
 /**
@@ -26,10 +27,15 @@ public class MirrorService extends CarActivityService {
 		super.onDestroy();
 	}
 
-	// NEW SWIPE-AWAY KILL SWITCH
 	@Override
 	public void onTaskRemoved(Intent rootIntent) {
 		super.onTaskRemoved(rootIntent);
+
+		if (PermataApplication.get().isConnectedToAuto()) {
+			Log.i("App swiped away, but Android Auto is ACTIVE. Ignoring shutdown.");
+			return;
+		}
+
 		Log.i("App swiped away from Recents. Killing MirrorService.");
 		
 		if (md != null) {
