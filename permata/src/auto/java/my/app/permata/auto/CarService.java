22a1b298e0bd64c7ceafa437fc821de070ba18af
sklchan77 <163995725+sklchan77@users.noreply.github.com>
2026-08-17 12:47:59 +0800

    Update CarService.java

--- permata/src/auto/java/my/app/permata/auto/CarService.java
+++ permata/src/auto/java/my/app/permata/auto/CarService.java
@@ -3,6 +3,7 @@ package my.app.permata.auto;
 import com.google.android.apps.auto.sdk.CarActivity;
 import com.google.android.apps.auto.sdk.CarActivityService;
 
+import my.app.permata.media.service.PermataMediaService;
 import my.app.permata.media.service.PermataMediaServiceConnection;
 import my.app.permata.media.service.MediaSessionCallback;
 import my.app.utils.log.Log;
@@ -21,6 +22,11 @@ public class CarService extends CarActivityService {
 	public void onCreate() {
 		Log.d("Creating CarService: " + this);
 		super.onCreate();
+		
+		// WAKE UP HEADLESS AUDIO ENGINE AND HIJACK FOCUS
+		// This forces the media service to launch in the background the exact moment Permata Auto is opened on the IHU.
+		Log.i("CarService started: Waking up PermataMediaService as a headless engine.");
+		PermataMediaService.requestFocusAndAnchor(this);
 	}
 
 	@Override
